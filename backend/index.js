8d0e68460279840c859ea1ed1250c67c32860e57
Christian Yitzak <christianyitzak@icloud.com>
2026-04-07 10:52:49 +0700

    make connection to mongodb database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log('MongoDB Connected..'))
+    .catch((err) => console.log('MongoDB Connection failed!'))
 
 app.use(express.json());
 
