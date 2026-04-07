04898e423125a79417893c11881cc7925b697e05
Christian Yitzak <christianyitzak@icloud.com>
2026-04-07 10:50:19 +0700

    add .env package for storing credentials

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`Server is running at port ${port}...`);
 });
